5251b2c77b01ae58091c97b7096438fe795b7258
minseo Jo <liging12@naver.com>
2024-10-20 02:52:00 +0900

    feat: MessageSourceConfig

---
+++ message_source/src/main/java/com/messagesource/MessageSourceConfig.java
@@ -0,0 +1,20 @@
+package com.messagesource;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MessageSourceConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages/messages");
+        messageSource.setCacheSeconds(3600);
+        return messageSource;
+    }
+}
